1a576a6839a04bc4409d8583b9005c5bad608613
girija.a <girija.a@zohocorp.com>
2021-03-23 20:31:12 +0530

    [spring-movement]added sample api using spring and its changes.

---
+++ src/main/java/com/galata/apis/controller/SampleController.java
@@ -0,0 +1,24 @@
+package com.galata.apis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by girija-4135 on 22/03/21
+ */
+
+/**
+ * Reference to write CRUD api :
+ *  https://howtodoinjava.com/spring-rest/spring-rest-crud-jpa-example/
+ *
+ */
+@RestController
+@RequestMapping(value = "/galata/v1/example")
+public class SampleController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String get(){
+        return "hello from SPRING!!!";
+    }
+
+}
